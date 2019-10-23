2dff279146abedc4c6f854214a25bf42aba5eb8f
hugopassos <hugo.passos@hotmail.com>
2019-10-23 18:02:02 -0300

    Fixing stickler errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :post
-	has_many :comment
-	validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
-	validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: true
+   has_many :post
+	 has_many :comment
+	 validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
+	 validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: true
 end
