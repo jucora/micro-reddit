117881121bb150f2ac62f0c969e825926eb55cbb
hugopassos <hugo.passos@hotmail.com>
2019-10-23 18:14:02 -0300

    Fixing stickler errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   has_many :post
-	has_many :comment
-	validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
-	validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: true
+  has_many :comment
+  validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
+  validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: true
 end
