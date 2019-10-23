c84a39c9e6f021d8c338105db98725bdcdec93ce
hugopassos <hugo.passos@hotmail.com>
2019-10-23 14:44:59 -0300

    Created the model Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :post
 	validates :name, length: { minimum: 3 }, presence: true, uniqueness: true
-	validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: true 
+	validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: true
 end
